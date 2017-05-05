bb901c3e09439f4094356407b095d24da5c20e51
index1597 <aleksandrboris@MacBook-Pro-Aleksandr.local>
2017-05-05 18:07:58 +0300

    Layout and adaptive header, mobile menu, main banner and 'about zergtech' of the block

--- src/js/external.js
+++ src/js/external.js
@@ -1,3 +1,2 @@
 @@include('./partials/_bootstrap.js');
-@@include('./partials/_sticky.js');
 @@include('../../bower_components/slick-carousel/slick/slick.min.js');
--- src/js/internal.js
+++ src/js/internal.js
@@ -8,11 +8,38 @@
     };
     var elements = {
         siteHeader: $('.site-header'),
-        iconMenu: $('.icon-menu')
+        iconMenu: $('.icon-menu'),
+        mobileMenu: $('.mobile-menu'),
+        overlay: $('.overlay')
     };
     var options = {
+
     };
 
+    elements.iconMenu.on('click', function () {
+       elements.mobileMenu.toggleClass('active');
+       elements.overlay.toggleClass('active');
+       $('body').toggleClass('no-scroll');
+    });
 
+    elements.overlay.on('click', function () {
+        $(this).removeClass('active');
+        elements.mobileMenu.removeClass('active');
+        $('body').removeClass('no-scroll');
+    });
+
+    elements.mobileMenu.find('.has-child > a').on('click', function () {
+        var parent = $(this).parent();
+
+        if(parent.hasClass('active')) {
+            parent.removeClass('active').closest('.mobile-menu-middle').removeClass('submenu-active');
+            elements.mobileMenu.find('.menu-parent > li').not(parent).fadeIn();
+            parent.find('.submenu').fadeOut();
+        } else {
+            parent.addClass('active').closest('.mobile-menu-middle').addClass('submenu-active');
+            elements.mobileMenu.find('.menu-parent > li').not(parent).fadeOut();
+            parent.find('.submenu').fadeIn();
+        }
+    });
 
 })(jQuery);
